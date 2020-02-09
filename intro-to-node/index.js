19fb138d854cb867fd3485156159b527486c4ae8
Garima Singh <garingh128@gmail.com>
2020-02-09 16:45:53 +0530

    intro to node

---
+++ intro-to-node/index.js
@@ -0,0 +1,9 @@
+//jshint esversion:6
+var superheroes = require("superheroes");
+var mySuperheroName = superheroes.random();
+console.log(mySuperheroName);
+
+//jshint esversion:6
+var supervillains = require("supervillains");
+var mySupervillainName = supervillains.random();
+console.log(mySupervillainName);
